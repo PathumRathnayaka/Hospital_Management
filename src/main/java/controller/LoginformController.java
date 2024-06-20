d605b6dc4bdea6428aa5f8c4478a87ce1d7637d4
Pathum <thilinapathumrathnayaka@gmail.com>
2024-06-20 23:32:07 +0530

    login form done

--- src/main/java/controller/LoginformController.java
+++ src/main/java/controller/LoginformController.java
@@ -1,4 +1,33 @@
 package controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
+
 public class LoginformController {
+
+    @FXML
+    private ToggleGroup accountType;
+
+    @FXML
+    private JFXRadioButton rBtnAccount;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXPasswordField txtPWD;
+
+    @FXML
+    void signUpOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signinOnAction(ActionEvent event) {
+
+    }
 }
